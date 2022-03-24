91d2c31a1661dd276c26f4ef3065733a66644352
IngridHernandez <60713446+IngridHernandez@users.noreply.github.com>
2022-03-23 17:56:42 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+//Global variables
+
+var employeeID;
+var fullName;
+var email;
+var extension;
+var dataList;
+
+employeeID = window.getElementById("employeeID");
+fullName =   window.getElementById("fullName");
+email =      window.getElementById("email");
+extension =  window.getElementById("extension");
+dataList =   window.getElementById("dataList");
